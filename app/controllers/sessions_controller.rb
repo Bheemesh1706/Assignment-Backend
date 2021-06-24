5a7768f23346ee9c4d95b17da54d95b520f477ec
Bheemesh1706 <bheemesh.18bce7170@vitap.ac.in>
2021-06-24 16:07:32 +0530

    Login, Cors done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+    def create 
+      
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] =  user.id
+            render json: {success_message: "Success!"}, status: 200
+        else
+            render json: {error_message: "User name or Password Invalid"}, status: 420
+        end
+ 
+    end
+
+    def session_user
+        if session
+            data = session[:agency_id]
+            render json: data
+        else
+            render json:  {error_message: "No seesion availabel"}, status: 420
+        end
+    end
+ 
+    def destroy
+        session[:user_id]= nil
+        render json: {success_message: "Successfully Logout"} 
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,20 @@
 class UsersController < ApplicationController
-    def create 
-        
-        user = User.find_by(username: params[:user][:username].downcase)
-        if user && user.authenticate(params[:user][:password])
-            session[:user_id] =  user.id
-            render json: {success_message: "Success!"}, status: 200
-        else
-            render json: {error_message: "User name or Password Invalid"}, status: 420
-        end
- 
-    end
 
-    def session_user
-        if session
-            data = session[:agency_id]
-            render json: data
-        else
-            render json:  {error_message: "No seesion availabel"}, status: 420
+    def index
+        @users = User.all
+    
+        render json: @users
+    end
+    
+    
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_user
+        @user = User.find(params[:id])
         end
-    end
- 
-    def destroy
-        session[:user_id]= nil
-        render json: {success_message: "Successfully Logout"} 
-    end
+
+        # Only allow a trusted parameter "white list" through.
+        def user_params
+        params.require(:user).permit(:username,:password)
+        end
 end
